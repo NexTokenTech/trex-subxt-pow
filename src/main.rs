c940ff0f7b96bae34f4f636dc80b6990d48a5ec3
kaisuki <1103720832@qq.com>
2022-03-29 16:01:45 +0800

    couple of cipher with difficulty and release_block_num

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use subxt::{
     PairSigner,
 };
 
+use serde::{Deserialize, Serialize};
+
 #[subxt::subxt(runtime_metadata_path = "metadata.scale")]
 pub mod polkadot {}
 
@@ -21,17 +23,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?
         .to_runtime_api::<polkadot::RuntimeApi<DefaultConfig, DefaultExtra<DefaultConfig>>>();
 
-    let str = "second vec u8 message2".as_bytes();
+    let str = self::construct_data();
+
     let hash = api
         .tx()
         .capsule_module()
-        .send_capsule_data(acount_id,str.to_vec(),1023)
+        .send_capsule_data(acount_id,str)
         .sign_and_submit(&signer)
         .await?;
 
-    println!("Capsule extrinsic submitted: {}", hash);
+    println!("Capsule extrinsic submitted: {:?}", hash);
 
     Ok(())
+}
+
+fn construct_data() -> Vec<u8>{
+    let cipher_text = "second vec u8 message".as_bytes().to_vec();
+    let cipher = Cipher{
+        cipher_text,
+        difficulty:32,
+        release_block_num:90
+    };
+
+    let cipher_text1 = "second vec u8 message1".as_bytes().to_vec();
+    let cipher1 = Cipher{
+        cipher_text:cipher_text1,
+        difficulty:32,
+        release_block_num:92
+    };
+
+    let mut trasactions = vec![];
+    trasactions.push(cipher);
+    trasactions.push(cipher1);
+
+    let transcode = serde_json::to_string(&trasactions).unwrap_or("".to_string());
+    println!("transcode: {:?}", transcode);
+    transcode.as_bytes().to_vec()
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct Cipher{
+    cipher_text:Vec<u8>,
+    difficulty:u32,
+    release_block_num:u32
 }
 
 
